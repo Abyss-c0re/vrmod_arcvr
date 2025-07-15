9ad6115c70685f4b0ab836435533f7a7a67ad1f3
catoni0 <mr.vladimir1337@gmail.com>
2025-07-15 15:18:18 +0300

    knife integration

--- lua/weapons/arcticvr_knife/shared.lua
+++ lua/weapons/arcticvr_knife/shared.lua
@@ -23,11 +23,6 @@ SWEP.Slot = 0
 SWEP.SlotPos = 1
 SWEP.DrawAmmo = false
 SWEP.DrawCrosshair = false
-SWEP.MeleeAttack = true -- whether this weapon can melee attack
-SWEP.MeleeVelThreshold = 3 -- how fast you have to swing
-SWEP.MeleeDelay = 0.25
-SWEP.MeleeDamage = 10
-SWEP.MeleeDamageType = DMG_SLASH
 SWEP.MeleeHitSound = "weapons/knife/knife_hitwall1.wav"
 SWEP.MeleeStrikeSound = "weapons/knife/knife_hit1.wav"
 SWEP.NotAGun = true
@@ -43,4 +38,35 @@ if CLIENT then
         bladeend = 1,
         bladestart = 2,
     }
+end
+
+function SWEP:Initialize()
+    if SERVER then
+        hook.Add("VRMod_MeleeHit", "ArcticVR_Knife_Hit_" .. tostring(self:EntIndex()), function(hitData, callback)
+            local ply = hitData.Attacker
+            if IsValid(ply) and IsValid(ply:GetActiveWeapon()) and ply:GetActiveWeapon() == self then
+                callback(nil, "Impact.Metal", -- Custom decal for crowbar
+                    nil, -- Keep default damage
+                    nil, -- Keep default multiplier (1.25 for blunt)
+                    nil, -- Keep default damage type
+                    hitData.Reach, -- Keep default reach 
+                    hitData.Radius, -- Keep default radius
+                    "sharp")
+            end
+        end)
+    end
+end
+
+function SWEP:OnRemove()
+    hook.Remove("VRMod_MeleeHit", "ArcticVR_Knife_Hit_" .. tostring(self:EntIndex()))
+end
+
+-- Prevent default gun click sound
+function SWEP:PrimaryAttack()
+    -- Do nothing to suppress default firing behavior and click sound
+end
+
+-- Prevent default gun click sound
+function SWEP:SecondaryAttack()
+    -- Do nothing to suppress default firing behavior and click sound
 end
