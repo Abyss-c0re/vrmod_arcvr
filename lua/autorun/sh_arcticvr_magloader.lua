8bfa47fde5f5bf0cfcaae112b68658f8d5a3fea7
Abyss-c0re <dev@abyss-core.com>
2026-04-30 00:35:50 +0300

    cleanup

--- lua/autorun/sh_arcticvr_magloader.lua
+++ lua/autorun/sh_arcticvr_magloader.lua
@@ -27,27 +27,6 @@ function ArcticVR.LoadMagazineType(mag)
 	end
 end
 
--- function ArcticVR.LoadentityType(mag)
--- ArcticVR.MagazineTable[mag.Name] = mag
--- ArcticVR.NumMagazines = ArcticVR.NumMagazines + 1
--- if ArcticVR.GenerateMagEntities then
--- if !(dnt_types[mag.Type] or mag.DoNotRegister) and mag.Model then
--- local magent = {}
--- magent.Base = "arcticvr_mag"
--- magent.PrintName = mag.PrintName
--- magent.Spawnable = true
--- magent.Category = "Arctic VR - Magazines"
--- magent.Rounds = mag.Capacity
--- magent.ArcticVR = true
--- magent.ArcticVRMagazine = true
--- magent.MagID = mag.Name
--- for i, k in pairs(mag) do
--- magent[i] = k
--- end
--- scripted_ents.Register( magent, "" .. mag.Name )
--- end
--- end
--- end
 for k, v in pairs(file.Find("arcticvr/magazines/*", "LUA")) do
 	include("arcticvr/magazines/" .. v)
 	AddCSLuaFile("arcticvr/magazines/" .. v)
